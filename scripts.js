e4628747a2b335ba0f5a621f811f52844d2756bc
Luqman <bluebearymuffin55@gmail.com>
2023-09-11 09:34:03 +0800

    Create a constructor func to create books object

--- scripts.js
+++ scripts.js
@@ -1,2 +1,11 @@
 const myLibrary = [];
 
+function Book(title, author, pages, read) {
+  this.title = title
+  this.author = author
+  this.pages = pages
+  this.read = read
+  this.info = function() {
+    return `${title} by ${author}, ${pages}, ${read}`
+  }
+}
